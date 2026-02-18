132d08f4b107513f206a49d126fcca44db2e1044
Fariz Adzmir <farizadzmir@gmail.com>
2026-02-18 23:26:51 +0800

    Fix: Correct Lenis options in SmoothScroll.tsx

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -13,10 +13,9 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
+      orientation: "vertical",
+      gestureOrientation: "vertical",
       smoothWheel: true,
-      webgl: false,
     });
 
     lenis.on("scroll", ScrollTrigger.update);
